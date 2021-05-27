26646905878bec77d6b128cd32c958e9645ee5e7
misha trunev <nove48@yandex.ru>
2021-05-27 18:22:12 +0300

    создание классов и установка зависимостей между ними

---
+++ Service/Infrastructure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Infrastructure
+    {
+        public static List<Rule> FillList(string[] arr)
+        {
+            List <Rule>res= new List<Rule>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res.Add(new Rule { Name = arr[i].Split(' ').First(), Rules=AddRules(arr[i].Split(' ')) });
+            }
+            return res;
+        }
+        private static List<string> AddRules(string[] str)
+        {
+            List<string> res = new List<string>();
+            for (int i = 1; i < str.Length; i++)
+            {
+                res.Add(str[i]);
+            }
+            return res;
+        }
+        public static List<string> Search(List<Rule> rules)
+        {
+            return null;
+        }
+    }
+}
--- Сs-grammar/Form1.cs
+++ Сs-grammar/Form1.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Service;
 namespace Сs_grammar
 {
     public partial class Form1 : Form
@@ -17,5 +16,25 @@ namespace Сs_grammar
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] arrstring = textBox1.Text.Replace("\r", "").Split('\n');
+            formater(arrstring);
+            List<Rule> list = Infrastructure.FillList(arrstring);
+        }
+        private void formater(string[] str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = str[i].Replace(" ", "");
+                str[i] = str[i].Replace("-", " ");
+                str[i] = str[i].Replace("|", " ");
+            }
+        }
     }
 }
